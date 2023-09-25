0c19a17e637fcff52fee3baf317e57bd0bc6a4e3
peshraw <90914925+peshr4w@users.noreply.github.com>
2023-09-25 15:42:23 +0300

    g

--- src/App.vue
+++ src/App.vue
@@ -178,7 +178,7 @@
             class="w-full bg-white rounded-full p-1"
           />
         </div>
-         <div class="cursor-pointer transition hover:-translate-y-2 w-10">
+        <div class="cursor-pointer transition hover:-translate-y-2 w-10">
           <img
             src="https://www.svgrepo.com/show/353751/flutter.svg"
             alt="flutter"
@@ -309,8 +309,98 @@
       d="M0,80L80,88C160,96,320,112,480,104C640,96,800,64,960,50.7C1120,37,1280,43,1440,48C1600,53,1760,59,1920,66.7C2080,75,2240,85,2400,85.3C2560,85,2720,75,2880,69.3C3040,64,3200,64,3360,72C3520,80,3680,96,3840,101.3C4000,107,4160,101,4320,82.7C4480,64,4640,32,4800,24C4960,16,5120,32,5280,45.3C5440,59,5600,69,5760,66.7C5920,64,6080,48,6240,37.3C6400,27,6560,21,6720,29.3C6880,37,7040,59,7200,64C7360,69,7520,59,7680,69.3C7840,80,8000,112,8160,112C8320,112,8480,80,8640,80C8800,80,8960,112,9120,122.7C9280,133,9440,123,9600,109.3C9760,96,9920,80,10080,82.7C10240,85,10400,107,10560,114.7C10720,123,10880,117,11040,114.7C11200,112,11360,112,11440,112L11520,112L11520,160L11440,160C11360,160,11200,160,11040,160C10880,160,10720,160,10560,160C10400,160,10240,160,10080,160C9920,160,9760,160,9600,160C9440,160,9280,160,9120,160C8960,160,8800,160,8640,160C8480,160,8320,160,8160,160C8000,160,7840,160,7680,160C7520,160,7360,160,7200,160C7040,160,6880,160,6720,160C6560,160,6400,160,6240,160C6080,160,5920,160,5760,160C5600,160,5440,160,5280,160C5120,160,4960,160,4800,160C4640,160,4480,160,4320,160C4160,160,4000,160,3840,160C3680,160,3520,160,3360,160C3200,160,3040,160,2880,160C2720,160,2560,160,2400,160C2240,160,2080,160,1920,160C1760,160,1600,160,1440,160C1280,160,1120,160,960,160C800,160,640,160,480,160C320,160,160,160,80,160L0,160Z"
     ></path>
   </svg>
-  <div class="h-56 bg-violet-600 pt-5 relative box-content " style="height: 200vh">
-    <div  data-aos="fade-up" class="bg-white rounded-2xl transition m-5 h-60 w-full sticky top-20" id="box"></div>
+  <div
+    class="h-56 bg-violet-600 pt-5 relative overflow-hidden"
+    style="height: 200vh"
+  >
+    <h1 data-aos="fade-up-right" class="text-white text-3xl ml-5">How I'm?</h1>
+    <div
+      data-aos="fade-up-left"
+      class="bg-white rounded-3xl transition m-5 p-3 mb-14 flex relative"
+      id="box"
+    >
+      <div
+        data-aos="zoom-in-up"
+        class="image bg-slate-200 shadow w-36 h-36 rounded-full overflow-hidden shrink-0"
+      >
+        <img
+          src="https://i.pinimg.com/564x/19/c0/df/19c0dfb48482cae6c971ef93dcf161f5.jpg"
+          alt=""
+          class="w-full"
+        />
+      </div>
+      <div data-aos="fade-up" class="container p-2 pl-3">
+        <h1 class="capitalize text-2xl text-slate-800 mb-2">peshraw jwamer</h1>
+        <p class="text-sm text-slate-600 mb-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+          quae. Ducimus officiis, molestiae repudiandae ad eos sint debitis
+          adipisci saepe nulla possimus velit neque labore voluptas numquam quod
+          libero reprehenderit?
+          <span class="inline-flex"
+            ><a href="#" class="text-blue-500"
+              >See my CV <sup>&#8599;</sup></a
+            ></span
+          >
+        </p>
+        <div class="flex items-center gap-x-2 justify-end">
+          <a class="w-8">
+            <img
+              src="https://www.svgrepo.com/show/349552/viber.svg"
+              alt=""
+              class="w-full"
+            />
+          </a>
+          <a class="w-6">
+            <img
+              src=" https://www.svgrepo.com/show/475692/whatsapp-color.svg"
+              alt=""
+              class="w-full"
+            />
+          </a>
+          <button class="w-6">
+            <img
+              src="https://www.svgrepo.com/show/303107/facebook-messenger-3-logo.svg"
+              alt=""
+              class="w-full"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="bg-slate-100">
+      <svg
+        id="wave"
+        style="transform: rotate(180deg); transition: 0.3s"
+        viewBox="0 0 1440 100"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+            <stop stop-color="rgba(248, 250, 252, 1)" offset="0%"></stop>
+            <stop stop-color="rgba(248, 250, 252, 1)" offset="100%"></stop>
+          </linearGradient>
+        </defs>
+        <path
+          style="transform: translate(0, 0px); opacity: 1"
+          fill="url(#sw-gradient-0)"
+          d="M0,0L34.3,10C68.6,20,137,40,206,43.3C274.3,47,343,33,411,33.3C480,33,549,47,617,58.3C685.7,70,754,80,823,76.7C891.4,73,960,57,1029,46.7C1097.1,37,1166,33,1234,33.3C1302.9,33,1371,37,1440,38.3C1508.6,40,1577,40,1646,48.3C1714.3,57,1783,73,1851,78.3C1920,83,1989,77,2057,73.3C2125.7,70,2194,70,2263,71.7C2331.4,73,2400,77,2469,73.3C2537.1,70,2606,60,2674,58.3C2742.9,57,2811,63,2880,56.7C2948.6,50,3017,30,3086,31.7C3154.3,33,3223,57,3291,70C3360,83,3429,87,3497,78.3C3565.7,70,3634,50,3703,38.3C3771.4,27,3840,23,3909,33.3C3977.1,43,4046,67,4114,63.3C4182.9,60,4251,30,4320,23.3C4388.6,17,4457,33,4526,46.7C4594.3,60,4663,70,4731,66.7C4800,63,4869,47,4903,38.3L4937.1,30L4937.1,100L4902.9,100C4868.6,100,4800,100,4731,100C4662.9,100,4594,100,4526,100C4457.1,100,4389,100,4320,100C4251.4,100,4183,100,4114,100C4045.7,100,3977,100,3909,100C3840,100,3771,100,3703,100C3634.3,100,3566,100,3497,100C3428.6,100,3360,100,3291,100C3222.9,100,3154,100,3086,100C3017.1,100,2949,100,2880,100C2811.4,100,2743,100,2674,100C2605.7,100,2537,100,2469,100C2400,100,2331,100,2263,100C2194.3,100,2126,100,2057,100C1988.6,100,1920,100,1851,100C1782.9,100,1714,100,1646,100C1577.1,100,1509,100,1440,100C1371.4,100,1303,100,1234,100C1165.7,100,1097,100,1029,100C960,100,891,100,823,100C754.3,100,686,100,617,100C548.6,100,480,100,411,100C342.9,100,274,100,206,100C137.1,100,69,100,34,100L0,100Z"
+        ></path>
+      </svg>
+      <div class="bg-slate-100 p-4 relative h-screen">
+        <h1 data-aos="fade-up-right" class="text-slate-800 text-3xl mb-5 relative">What I do?</h1>
+        <img src="https://www.svgrepo.com/show/400961/art-compass-divider-drawing-equipment-graphic.svg" alt="" class="w-3/12 rotate-45 opacity-50  right-0 absolute -top-10">
+        <div class="flex gap-x-2">
+          <div data-aos="fade-up" class="web rounded-2xl bg-white basis-1/2 p-2 capitalize h-56">
+            <h1 class="text-xl">web app</h1>
+          </div>
+          <div data-aos="fade-up" class="mob rounded-2xl bg-white basis-1/2 p-2 capitalize h-56">
+            <h1 class="text-xl">mobile app</h1>
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   <Teleport to="body">
     <Transition>
@@ -392,7 +482,7 @@ export default {
         message.value = "";
       }
     };
-    
+
     return { show, message, sendMessage, commentsStore, format };
   },
   data() {
